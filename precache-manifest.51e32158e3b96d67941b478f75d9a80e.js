a8a1ba11c5f3a292dcecce7b3e50d9019f3fca15
pjonp <pjonp01@gmail.com>
2019-12-12 20:22:08 -0500

    Updates

---
+++ precache-manifest.51e32158e3b96d67941b478f75d9a80e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd2ee0a2d653473e2a5895802fe61389",
+    "url": "/gsheet-cards/index.html"
+  },
+  {
+    "revision": "94bb0fb6dc4fd5b12966",
+    "url": "/gsheet-cards/static/css/main.057c7483.chunk.css"
+  },
+  {
+    "revision": "e047c609b90843f18a4e",
+    "url": "/gsheet-cards/static/js/2.41caa598.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/gsheet-cards/static/js/2.41caa598.chunk.js.LICENSE"
+  },
+  {
+    "revision": "94bb0fb6dc4fd5b12966",
+    "url": "/gsheet-cards/static/js/main.073978b5.chunk.js"
+  },
+  {
+    "revision": "c1f1187406e24906f742",
+    "url": "/gsheet-cards/static/js/runtime-main.cfa6e92e.js"
+  }
+]);
